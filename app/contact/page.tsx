f7820ba38ebe9eba692283ea5bc052f1ae4077a5
vbhv4GitHub <ravi9406126068@gmail.com>
2024-07-27 22:26:39 +0530

    chore(contact): :necktie: update cta button type to submit from button

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -55,8 +55,8 @@ export default function Contact() {
             </div>
             <div className='flex items-center justify-between'>
               <button
+                type='submit'
                 id='contact-form-cta'
-                type='button'
                 className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
               >
                 Send
